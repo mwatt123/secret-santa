21fe7c0f287fbfd578b2d4637ad1b101a0b2925b
Matthew Watt <matthew.watt@cover.com>
2019-11-10 18:49:11 -0500

    update token to be shorter

--- app/models/santa.rb
+++ app/models/santa.rb
@@ -1,8 +1,14 @@
-class Santa < ApplicationRecord
-  has_secure_token
-  
+require 'securerandom'
+
+class Santa < ApplicationRecord  
   belongs_to :family
   has_many   :gifts
   has_one    :gift_to_buy, -> { where(year: Time.current.year) }, class_name: 'Gift', foreign_key: :secret_santa_id
   has_one    :recipient, through: :gift_to_buy
+
+  before_create :save_token
+
+  def save_token
+    self.token ||= SecureRandom.urlsafe_base64(5)
+  end
 end
