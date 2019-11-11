2fb8b14357a00525f64d6f51b10570f386c67786
Matthew Watt <matthew.watt@cover.com>
2019-11-10 21:39:43 -0500

    touch to update timestamp when page viewed

--- app/controllers/santas_controller.rb
+++ app/controllers/santas_controller.rb
@@ -3,6 +3,7 @@ class SantasController < ApplicationController
     @santa = Santa.find_by(token: params[:token])
     render_not_found if @santa.blank?
 
+    @santa.touch
     @recipient = @santa.recipient
   end
 end
