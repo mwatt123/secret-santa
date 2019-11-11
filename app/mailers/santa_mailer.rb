6927ecc1413e791f1eae29fc88f6e38854c1f3b5
Matthew Watt <matthew.watt@cover.com>
2019-11-10 21:02:31 -0500

    update email subject for clarity

--- app/mailers/santa_mailer.rb
+++ app/mailers/santa_mailer.rb
@@ -2,6 +2,6 @@ class SantaMailer < ApplicationMailer
   def notification_email(santa)
     @name = santa.name
     @url  = "https://www.matthew-watt.com/santas/#{santa.token}"
-    mail(to: santa.email, subject: "#{santa.name} - Watt Family Secret Santa")
+    mail(to: santa.email, subject: "#{santa.name} - Family Secret Santa")
   end
 end
--- test/mailers/previews/santa_mailer_preview.rb
+++ test/mailers/previews/santa_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/santa_notifier_mailer
 class SantaMailerPreview < ActionMailer::Preview
   def notification_email
-    SantaMailer.with(santa: Santa.last).notification_email
+    SantaMailer.notification_email(Santa.last)
   end
 end
