f4846eda3a294c1f16afa884df6c0d721beb9b3e
Matthew Watt <matthew.watt@cover.com>
2019-11-10 20:45:12 -0500

    update email method to expect an argument

--- app/mailers/santa_mailer.rb
+++ app/mailers/santa_mailer.rb
@@ -1,6 +1,6 @@
 class SantaMailer < ApplicationMailer
-  def notification_email
-    @santa = params[:santa]
+  def notification_email(santa)
+    @santa = santa
     @url   = "https://www.matthew-watt.com/santas/#{@santa.token}"
     mail(to: @santa.email, subject: 'Watt Family Secret Santa')
   end
